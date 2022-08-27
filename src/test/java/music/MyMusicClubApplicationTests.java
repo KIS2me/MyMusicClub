bd21660c93d5f001be2d6f10c9dcba742ec288fa
TuXin <1121461399@qq.com>
2022-08-27 11:55:37 +0800

    引入了RabbitMQ依赖，并对在application.yml中进行了配置

--- src/test/java/music/MyMusicClubApplicationTests.java
+++ src/test/java/music/MyMusicClubApplicationTests.java
@@ -2,6 +2,8 @@ package music;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.springframework.amqp.core.AmqpAdmin;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 
